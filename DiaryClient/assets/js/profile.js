6c2f56e9b79803e539c779b57b5987b589f7317f
Nguyen Luong Khang <96479575+Khangnekk@users.noreply.github.com>
2024-07-10 23:08:52 +0700

    edit/delete post

--- DiaryClient/assets/js/profile.js
+++ DiaryClient/assets/js/profile.js
@@ -1,13 +1,12 @@
 $(document).ready(function () {
     var token = localStorage.getItem("token");
     var username = localStorage.getItem("username");
-
     var avatarUrl = `http://localhost:5109/api/User/GetAvatar/${username}`;
     if (avatarUrl == `http://localhost:5109/api/User/GetAvatar/null`)
         avatarUrl = `https://t4.ftcdn.net/jpg/05/49/98/39/360_F_549983970_bRCkYfk0P6PP5fKbMhZMIb07mCJ6esXL.jpg`
 
-    
-    
+
+
 
 
     // Lấy ra tất cả các post của tôi (cả public và private)
@@ -46,12 +45,10 @@ $(document).ready(function () {
                                                 <button id="btnMore-${post.id}">⋮</button>
                                             </div>
                                         </div>
-                                        
-                                        
                                     </div>
                                     <div class="post-body">
-                                        <h3 class="post-title" name="title">${post.title}</h3>
-                                        <p class="post-content" name="content">${post.content}</p>
+                                        <h3 class="post-title" name="title" id="title-post-${post.id}">${post.title}</h3>
+                                        <p class="post-content" name="content" id="content-post-${post.id}">${post.content}</p>
                                     </div>
                                 </div>
                                 <div class="comment-section" id="comments-${post.id}">
@@ -71,7 +68,7 @@ $(document).ready(function () {
                             </div>
                         `;
                 $('#mypostSection').append(postElement);
-                popup(post.id)
+                popup(post)
                 loadComments(post.id);
             });
         },
@@ -140,33 +137,124 @@ $(document).ready(function () {
         });
     }
 
-    function popup(id) {
-        $(document).on('click', `#btnMore-${id}`, function () {
+    function popup(post) {
+        $(document).on('click', `#btnMore-${post.id}`, function () {
             // Tạo popup menu nếu chưa tồn tại
-            if ($(`#dropdownMenuBtnMore-${id}`).length === 0) {
+            if ($(`#dropdownMenuBtnMore-${post.id}`).length === 0) {
                 var popup = `
-                    <div class="dropdown-menu-btn-more" id="dropdownMenuBtnMore-${id}" style="display: block; position: absolute; width: fit-content">
-                        <a href="profile.html">Edit</a>
-                        <a href="setting.html">Delete</a>
+                    <div class="dropdown-menu-btn-more" id="dropdownMenuBtnMore-${post.id}" style="display: block; position: absolute; width: fit-content">
+                        <a id="edit-post-${post.id}">Edit</a>
+                        <a id="delete-post-${post.id}">Delete</a>
                     </div>
                 `;
                 $(this).append(popup);
-    
+
                 // Cập nhật vị trí của popup menu để nó nằm ngay bên cạnh nút "More"
                 var offset = $(this).offset();
-                $(`#dropdownMenuBtnMore-${id}`).css({
+                $(`#dropdownMenuBtnMore-${post.id}`).css({
                     top: offset.top - $(this).outerHeight(),
                     left: offset.left
                 });
+
+                // Bắt sự kiện click btn-edit của post này
+                $(document).on('click', `#edit-post-${post.id}`, function () {
+                    var popupEdit = `
+                        <div id="editPostDialog" style="display: block;">
+                            <div>
+                                <form id="editPostForm">
+                                    <label for="postTitle">Title</label>
+                                    <input type="text" id="postTitleEdit" value="${post.title}"><br>
+                                    <label for="postContent">Content</label>
+                                    <textarea id="postContentEdit" placeholder="${post.content}"></textarea><br>
+                                    <label for="postIsPublic">Is Public</label>
+                                    <select id="postIsPublicEdit" value="${post.isPublic}">
+                                        <option value="true">Public</option>
+                                        <option value="false">Private</option>
+                                    </select><br>
+                                    <button type="button" id="savePostChanges">Save changes</button>
+                                    <button type="button" id="cancelChanges">Cancel</button>
+                                </form>
+                            </div>
+                        </div>
+                    `; 
+                    $("#mypostSection").append(popupEdit);
+                    
+                    // Nếu ấn Save changes thì sẽ gọi api
+                    $(document).on('click', `#savePostChanges`, function () {
+                        var updateTitle = $("#postTitleEdit").val();
+                        var updateContent = $("#postContentEdit").val();
+                        updateIsPublic = ($("#postIsPublicEdit").val() == "true")?true:false;
+
+                        if(updateContent.length == 0) 
+                            updateContent = post.content;
+                        $.ajax({
+                            url: `http://localhost:5109/api/Post/UpdatePost`,
+                            type: 'PUT',
+                            headers: {
+                                'Authorization': 'Bearer ' + token,
+                                'Content-Type': 'application/json'
+                            },
+                            data: JSON.stringify({
+                                id: post.id,
+                                title: updateTitle,
+                                content: updateContent,
+                                isPublic: updateIsPublic
+                            }),
+                            success: function (responsePostEdit) {
+                                alert(`Update post with id ${responsePostEdit.id} successfully`)
+                                $(`#title-post-${post.id}`).html(responsePostEdit.title) 
+                                $(`#content-post-${post.id}`).html(responsePostEdit.content) 
+                                $("#editPostDialog").remove();
+                                var audio = document.getElementById('tengSound');
+                                audio.play();
+                            },
+                            error: function (err) {
+                                console.log('Error:', err);
+                                reject(err);
+                            }
+                        })
+                    });
+                    // Nếu ấn Cancel thì remove popup
+                    $(document).on('click', `#cancelChanges`, function () {
+                        alert('There are no changes to this article');
+                        $("#editPostDialog").remove();
+                    });
+                })
+
+                // Bắt sự kiện click btn-delete của post này
+                $(document).on('click', `#delete-post-${post.id}`, function () {
+                    var isDelete = confirm('Are you sure to delete this post');
+                    // Nếu người dùng bấm ok thì mới call api
+                    if (isDelete) {
+                        $.ajax({
+                            url: `http://localhost:5109/api/Post/DeletePost/${post.id}`,
+                            type: 'DELETE',
+                            headers: {
+                                'Authorization': 'Bearer ' + token
+                            },
+                            success: function (responsePostDelete) {
+                                alert(`Delete post with id ${post.id} successfully`)
+                                var audio = document.getElementById('tengSound');
+                                audio.play();
+                                $(`#post-${post.id}`).remove();
+                            },
+                            error: function (err) {
+                                console.log('Error:', err);
+                                reject(err);
+                            }
+                        })
+                    }
+                })
+
             } else {
-                $(`#dropdownMenuBtnMore-${id}`).toggle();
+                $(`#dropdownMenuBtnMore-${post.id}`).toggle();
             }
         });
-    
-        $(document).on('mouseleave', `#btnMore-${id}, #dropdownMenuBtnMore-${id}`, function (event) {
+
+        $(document).on('mouseleave', `#btnMore-${post.id}, #dropdownMenuBtnMore-${post.id}`, function (event) {
             // Kiểm tra nếu chuột không nằm trên nút hoặc menu thì ẩn menu đi
-            if (!$(event.relatedTarget).closest(`#btnMore-${id}`).length && !$(event.relatedTarget).closest(`#dropdownMenuBtnMore-${id}`).length) {
-                $(`#dropdownMenuBtnMore-${id}`).hide();
+            if (!$(event.relatedTarget).closest(`#btnMore-${post.id}`).length && !$(event.relatedTarget).closest(`#dropdownMenuBtnMore-${post.id}`).length) {
+                $(`#dropdownMenuBtnMore-${post.id}`).hide();
             }
         });
     }
